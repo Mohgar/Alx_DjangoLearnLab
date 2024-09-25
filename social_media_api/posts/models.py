5e22f981e56c58554a4d95370d4f2db991672afe
Mohgar <mohamedhgar007@gmail.com>
2024-09-25 13:22:41 +0300

    Add files via upload

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length= 150)
-    content = models.TextField
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now_add= True)
 
@@ -16,7 +16,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
